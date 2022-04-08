eef22fc3b9c6fe575cb4408290f9baec9951cef6
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-04-09 00:23:34 +0300

    betty style correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-
-/* betty style doc for function main goes there */
+/**
+ * main - function
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
