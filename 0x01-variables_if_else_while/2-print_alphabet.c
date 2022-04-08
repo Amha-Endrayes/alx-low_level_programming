a096d31a4a09507da0a876c9a8ec8377423e2f3a
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-04-09 00:55:46 +0300

    added answer to 3rd question

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	char goToSleep;
+	for (goToSleep = 'a' ; goToSleep <= 'z' ; goToSleep++) 
+	{
+	putchar (goToSleep);
+	}
+	return (0);
+}
