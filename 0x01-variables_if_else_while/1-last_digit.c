3053a63976bd132e3b05a55175cde6ac209852d5
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-04-09 00:28:05 +0300

    betty style correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  * main - function
  *
@@ -11,11 +9,11 @@
  */
 int main(void)
 {
-	int n;
+    int n;
     int y;
-    
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
     y = n % 10;
 
@@ -30,6 +28,6 @@ int main(void)
     else
     {
         printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
-    }            
-	return (0);
+    }
+    return (0);
 }
