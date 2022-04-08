f4620a00904e439279c12fadde953299c02b6210
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-04-09 01:53:53 +0300

    correction to question 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	char mirror;
 
-	for (mirror = 'z' ; mirror <= 'a' ; mirror--)
+	for (mirror = 'z' ; mirror >= 'a' ; mirror--)
 	{
 	putchar (mirror);
-	}
+	}s
 	putchar('\n');
 	return (0);
 }
