5e9c1e6cc132bf36a36919158391f0282a2329a9
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-04-12 04:18:31 +0300

    advanced task correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int space = ' ';
 
 for (a = '0'; a <= '9'; a++)
 {
-	for (b = '0'; b <= '8'; b++)
+	for (b = '0'; b <= '9'; b++)
 	{
 		for (c = '0'; c <= '9'; c++)
 		{
@@ -35,6 +35,7 @@ for (a = '0'; a <= '9'; a++)
 			}
 			else
 			{
+			putchar(space);				
 			}
 
 			}
