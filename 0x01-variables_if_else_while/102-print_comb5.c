76b3f7ea947730b30dc82542bc594288aed776e5
Amha-Endrayes <36323983+Amha-Endrayes@users.noreply.github.com>
2022-04-12 04:06:58 +0300

    betty style coding

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,38 +12,40 @@ int a, b, c, d;
 int comma = ',';
 int space = ' ';
 
-	for (a = '0'; a <= '9'; a++)
+for (a = '0'; a <= '9'; a++)
+{
+	for (b = '0'; b <= '9'; b++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (c = '0'; c <= '9'; c++)
 		{
-			for (c = '0'; c <= '9'; c++)
+		for (d = '0'; d <= '9'; d++)
+		{
+			if (a < c || (a == c && b < d))
 			{
-				for (d = '0'; d <= '9'; d++)
-				{
-					if (a < c || (a == c && b < d))
-					{
-						putchar(a);
-						putchar(b);
-						putchar(space);
-						putchar(c);
-						putchar(d);
-						if ((a + b + c + d) < '9' + '8' + '9' + '9')
-						{
-							putchar(comma);
-							putchar(space);
-						}
-						else
-						{
-						}
-					}
-					else
-					{
-					}
-				}
+			putchar(a);
+			putchar(b);
+			putchar(space);
+			putchar(c);
+			putchar(d);
+
+			if ((a + b + c + d) < '9' + '8' + '9' + '9')
+			{
+			putchar(comma);
+			putchar(space);
+			}
+			else
+			{
+			}
+			
+			}
+			else
+			{
 			}
 		}
+		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
 
